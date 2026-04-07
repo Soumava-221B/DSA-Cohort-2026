76c766a0b41a2629da5717c07b91d68d4817cee3
Soumava Das <soumavadas06@gmail.com>
2026-04-07 15:39:25 +0530

    Postorder traversal

---
+++ sessions/2026-04-03-session50/Solution_Tree_Postorder_traversal.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void traversal(TreeNode root, List<Integer> res)
+    {
+        if (root != null) {
+            traversal(root.left, res);
+            traversal(root.right, res);
+            res.add(root.val);
+        }
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root)
+    {
+        List<Integer> res = new ArrayList<>();
+        traversal(null, res);
+        return res;
+    }
+}
