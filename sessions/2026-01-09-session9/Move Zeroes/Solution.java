7cee39ba34b0e9a008207a6d5c110b640094a9a0
Soumava Das <soumavadas06@gmail.com>
2026-05-17 23:31:29 +0530

    Moves Zeroes

---
+++ sessions/2026-01-09-session9/Move Zeroes/Solution.java
@@ -0,0 +1,15 @@
+function moveZeroes(nums: number[]): void {
+    let insertPos = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[insertPos] = nums[i];
+            insertPos++;
+        }
+    }
+
+    while(insertPos < nums.length) {
+        nums[insertPos] = 0;
+        insertPos++;
+    }
+};
