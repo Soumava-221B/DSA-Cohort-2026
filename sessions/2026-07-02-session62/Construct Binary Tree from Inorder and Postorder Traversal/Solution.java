6e3791056eb0029cce3f5e30d807b651a3f0df7d
Soumava Das <soumavadas06@gmail.com>
2026-08-01 22:04:11 +0530

    Construct Binary tree from inorder and postorder traversal

---
+++ sessions/2026-07-02-session62/Construct Binary Tree from Inorder and Postorder Traversal/Solution.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    HashMap<Integer, Integer> map = new HashMap<>();
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+
+        return build(inorder, 0, inorder.length - 1,
+                     postorder, 0, postorder.length - 1);
+    }
+
+    private TreeNode build(int[] inorder, int inStart, int inEnd,
+                           int[] postorder, int postStart, int postEnd) {
+
+        if (inStart > inEnd || postStart > postEnd) {
+            return null;
+        }
+
+        int rootValue = postorder[postEnd];
+        TreeNode root = new TreeNode(rootValue);
+
+        int rootIndex = map.get(rootValue);
+        int leftSize = rootIndex - inStart;
+
+        root.left = build(inorder, inStart, rootIndex - 1,
+                          postorder, postStart, postStart + leftSize - 1);
+
+        root.right = build(inorder, rootIndex + 1, inEnd,
+                           postorder, postStart + leftSize, postEnd - 1);
+
+        return root;
+    }
+}
