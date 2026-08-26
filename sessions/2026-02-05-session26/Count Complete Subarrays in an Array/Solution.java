f02174f2652214998bc888543cb35e5675da4391
Soumava Das <soumavadas06@gmail.com>
2026-08-26 23:45:01 +0530

    Count Complete Subarray

---
+++ sessions/2026-02-05-session26/Count Complete Subarrays in an Array/Solution.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int countCompleteSubarrays(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int totalDistinct = set.size();
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int left = 0;
+        int count = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+
+            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
+
+            while (map.size() == totalDistinct) {
+
+                count += nums.length - right;
+                map.put(nums[left], map.get(nums[left]) - 1);
+
+                if (map.get(nums[left]) == 0) {
+                    map.remove(nums[left]);
+                }
+
+                left++;
+            }
+        }
+
+        return count;
+    }
+}
