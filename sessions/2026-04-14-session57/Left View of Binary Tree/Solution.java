fdb885e6028084f82a208df8ba0140a7b3205a7b
Soumava Das <soumavadas06@gmail.com>
2026-09-07 23:46:46 +0530

    Binary Tree

---
+++ sessions/2026-04-14-session57/Left View of Binary Tree/Solution.java
@@ -0,0 +1,45 @@
+/* Structure of Binary Tree Node
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int val) {
+        this.data = val;
+        this.left = this.right = null;
+    }
+}*/
+
+class Solution {
+    public ArrayList<Integer> leftView(Node root) {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                Node current = queue.poll();
+
+                if (i == 0) {
+                    result.add(current.data);
+                }
+
+                if (current.left != null) {
+                    queue.offer(current.left);
+                }
+
+                if (current.right != null) {
+                    queue.offer(current.right);
+                }
+            }
+        }
+
+        return result;
+    }
+}
